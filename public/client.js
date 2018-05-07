704a81be549b76d404d78b03f7d504623246a7ca
My Tran <myytran@thinkful.com>
2018-05-07 17:24:34 -0400

    add content to booking page

--- public/client.js
+++ public/client.js
@@ -1,1 +1,34 @@
+$(document).ready(function(){
 
+$(".startButton1").click(function(){
+  $("#wm-services").show();
+});
+
+$(".startButton2").click(function(){
+  $("#men-services").show();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}); //end of doc ready function
